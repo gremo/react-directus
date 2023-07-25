c31eb82b2e3079a845a432e7bf1302b3ee71bdf5
Sakuk <lukas.fritsch30@gmail.com>
2023-07-25 11:57:50 +0200

    Added uniform formatting and reformatted accordingly.

--- src/DirectusProvider.tsx
+++ src/DirectusProvider.tsx
@@ -13,21 +13,21 @@ import { DirectusImage } from './DirectusImage';
 export const DirectusContext = React.createContext<DirectusContextTpye | null>(null);
 
 export const DirectusProvider = ({ apiUrl, options, children }: DirectusProviderProps): JSX.Element => {
-  const value = React.useMemo<DirectusContextTpye>(() => ({
-    apiUrl: apiUrl,
-    directus: new Directus(apiUrl, options),
-    DirectusAsset: (props: DirectusAssetPropsContextualized<DirectusAssetProps>): JSX.Element =>
-      <DirectusAsset apiUrl={apiUrl} { ...props} />,
-    DirectusImage: (props: DirectusAssetPropsContextualized<DirectusImageProps>): JSX.Element =>
-      <DirectusImage apiUrl={apiUrl} { ...props} />
-  }), [apiUrl]);
-
-  return (
-    <DirectusContext.Provider value={value}>
-      {children}
-    </DirectusContext.Provider>
+  const value = React.useMemo<DirectusContextTpye>(
+    () => ({
+      apiUrl: apiUrl,
+      directus: new Directus(apiUrl, options),
+      DirectusAsset: (props: DirectusAssetPropsContextualized<DirectusAssetProps>): JSX.Element => (
+        <DirectusAsset apiUrl={apiUrl} {...props} />
+      ),
+      DirectusImage: (props: DirectusAssetPropsContextualized<DirectusImageProps>): JSX.Element => (
+        <DirectusImage apiUrl={apiUrl} {...props} />
+      ),
+    }),
+    [apiUrl]
   );
+
+  return <DirectusContext.Provider value={value}>{children}</DirectusContext.Provider>;
 };
 
-export const useDirectus = (): null | DirectusContextTpye =>
-  React.useContext(DirectusContext);
+export const useDirectus = (): null | DirectusContextTpye => React.useContext(DirectusContext);
