59875662b2f5145956cd8400ec0ac59b1a2d3ab3
Marco Polichetti <gremo1982@gmail.com>
2023-07-29 09:31:44 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 export { DirectusProvider, useDirectus } from '@/DirectusProvider';
-
-// components
 export { DirectusAsset } from '@components/DirectusAsset';
 export { DirectusImage } from '@components/DirectusImage';
