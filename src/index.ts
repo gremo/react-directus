660b286b3a4206d19080444dde1aa5989f2730b7
Sakuk <lukas.fritsch30@gmail.com>
2023-08-01 09:14:34 +0000

    rem public export

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,3 @@ export { DirectusProvider, useDirectus } from '@/DirectusProvider';
 export { DirectusAsset } from '@components/DirectusAsset';
 export { DirectusImage } from '@components/DirectusImage';
 export { useDirectusAuth } from '@hooks/useDirectusAuth';
-export { toID, generateAssetUrl } from '@utils/directus';
